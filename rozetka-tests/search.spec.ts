86a21bec2c78ddb862a1dd392211bc9e679fd95e
Igor Sadovyi <igor.sadovyi@shelf.io>
2024-10-18 06:50:38 +0200

    Enhance search tests with dynamic test data and additional assertions

--- rozetka-tests/search.spec.ts
+++ rozetka-tests/search.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('Search in rozetka', async ({ page }) => {
   await page.goto('https://rozetka.com.ua/ua/');
@@ -15,4 +15,48 @@ test('Search in rozetka', async ({ page }) => {
   await page.click(
     '//*[@class="header-search"]//button[contains(@class, "button") and contains(@class, "search-form__submit")]'
   );
+
+  const searchResult = page
+    .locator(
+      `//li[contains(@class, "catalog-grid")]//a[contains(@class, 'goods-tile__picture')]`
+    )
+    .first();
+
+  expect(searchResult.getAttribute('title')).toContain('iphone');
 });
+
+const testData = [
+  {
+    testId: 'MONE-001',
+    searchQuery: 'Монітори',
+    searchResultWord: 'Монітор',
+  },
+  {
+    testId: 'MONE-002',
+    searchQuery: "Комп'ютерна мишка",
+    searchResultWord: "Комп'ютерна мишка",
+  },
+];
+
+for (const data of testData) {
+  test.describe.serial('search suit', () => {
+    test(`${data.testId} search for "${data.searchQuery}" - should appear in search results`, async ({
+      page,
+    }) => {
+      await page.goto('https://rozetka.com.ua/ua/');
+      await page.locator(`//input[@name="search"]`).fill(data.searchQuery);
+
+      await page.keyboard.press('Enter');
+
+      const searchResult = page
+        .locator(
+          `//li[contains(@class, "catalog-grid")]//a[contains(@class, 'goods-tile__picture')]`
+        )
+        .first();
+
+      expect(searchResult.getAttribute('title')).toContain(
+        data.searchResultWord
+      );
+    });
+  });
+}
